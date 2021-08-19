a0feff336d5cee0dd54fda8876326aa87c4182e8
Philip Noebauer <pnoebauer>
2021-08-19 11:05:29 +0800

    setup conversion and routes

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,70 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
+	const galToL = 3.78541;
+	const lbsToKg = 0.453592;
+	const miToKm = 1.60934;
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
+	const conversionMap = {
+		km: {returnUnit: 'mi', convNumber: 1 / miToKm},
+		mi: {returnUnit: 'km', convNumber: miToKm},
+		l: {returnUnit: 'gal', convNumber: 1 / galToL},
+		gal: {returnUnit: 'l', convNumber: galToL},
+		kg: {returnUnit: 'lbs', convNumber: 1 / lbsToKg},
+		lbs: {returnUnit: 'kg', convNumber: lbsToKg},
+	};
+
+	const spellingMap = {
+		km: 'kilometers',
+		mi: 'miles',
+		l: 'liters',
+		gal: 'gallons',
+		kg: 'kilograms',
+		lbs: 'pounds',
+	};
+
+	// from query string => 3.1(initNum)
+	this.getNum = function (input) {
+		// const result = Number(input.match(/[0-9]+/g)[0]);
+		const result = Number(input.match(/[0-9]+/)[0]);
+
+		return result;
+	};
+
+	// from query string => 'mi'(initUnit)
+	this.getUnit = function (input) {
+		// const result = input.match(/[^0-9]+/g)[0];
+		const result = input.match(/[^0-9]+/)[0];
+
+		return result;
+	};
+
+	// 'km' (from 'mi')
+	this.getReturnUnit = function (initUnit) {
+		const result = conversionMap[initUnit].returnUnit;
+
+		return result;
+	};
+
+	this.spellOutUnit = function (unit) {
+		const result = spellingMap[unit];
+
+		return result;
+	};
+
+	// e.g. 3.1(initNum) 'mi'(initUnit) => returnNum
+	this.convert = function (initNum, initUnit) {
+		const result = conversionMap[initUnit].convNumber * initNum;
+
+		return result;
+	};
+
+	// '3.1 miles converts to 4.98895 kilometers'
+	this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+		const result = `${initNum} ${this.spellOutUnit(
+			initUnit
+		)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
+
+		return result;
+	};
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,27 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+	let convertHandler = new ConvertHandler();
 
+	// /api/convert?input=4gal
+	// { initNum: 3.1, initUnit: 'mi', returnNum: 4.98895, returnUnit: 'km', string: '3.1 miles converts to 4.98895 kilometers' }
+	// const x = convertHandler.convert(3.1, 'mi');
+
+	// console.log(convertHandler);
+
+	app.route('/api/convert/').get((req, res) => {
+		const {input} = req.query;
+		// console.log(req.query, input);
+
+		const initNum = convertHandler.getNum(input);
+		const initUnit = convertHandler.getUnit(input);
+		const returnUnit = convertHandler.getReturnUnit(initUnit);
+		const returnNum = convertHandler.convert(initNum, initUnit);
+		const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+		// console.log(initNum, initUnit, returnUnit, returnNum);
+		console.log(string);
+
+		return res.json({initNum, initUnit, returnNum, returnUnit, string});
+	});
 };
